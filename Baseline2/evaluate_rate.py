f253354bfbbf67c7b268ad110a8037ea2dea80f7
Akhilesh Somani <akhileshsomani1806@gmail.com>
2020-11-26 10:09:10 -0600

    Update evaluate_rate.py

--- Baseline2/evaluate_rate.py
+++ Baseline2/evaluate_rate.py
@@ -7,30 +7,22 @@ Evaluate the performance of Top-K recommendation:
 
 @author: hexiangnan
 '''
-import math
-import heapq # for retrieval topK
-import multiprocessing
+
 import numpy as np
-from time import time
-#from numba import jit, autojit
 from sklearn.metrics import mean_squared_error, r2_score
 
 def evaluate_rate_model(model, Review_test, Label_test, num_thread):
     """
-    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
     Return: score of each test rating.
     """
-        
-    mse, r2 = [],[]
     
     users,items = [],[]
     for i in range(len(Review_test)):
         users.append(Review_test[i][0])
         items.append(Review_test[i][1])
         
-    predictions = model.predict([users, items], 
-                                 batch_size=100, verbose=0)
-    
+    predictions = model.predict([np.array(users), np.array(items)], batch_size=1000, verbose=0)
+
     y_test = np.array(Label_test)*5
     yhat = np.array(predictions)*5
     assert len(predictions) == len(Label_test)
